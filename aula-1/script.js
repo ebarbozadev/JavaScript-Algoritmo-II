a8c511b4fbf47da606a98a98775f612bffa24d32
E.Barboza <e.barboza@edu.unipar.br>
2023-02-09 02:43:22 -0300

    Adicionando arquivos

---
+++ aula-1/script.js
@@ -0,0 +1,11 @@
+const empresas = require('./arrays');
+
+for(empresa in empresas){
+    for(let nEmpresa = 0; nEmpresa < 1; nEmpresa++){
+        for(let i = 0; i < empresas[empresa].length; i++){
+            console.log(
+                `A empresa ${empresa} tem o livro ${empresas[empresa][i].titulo} por ${empresas[empresa][i].preco}`
+            );
+        }
+    }
+}
